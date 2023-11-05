8552b390eb5df4422e2e2ab0f38d38cb82cf2088
BHASVIC-JackStapleton07 <jack.stapleton23@students.bhasvic.ac.uk>
2023-11-05 15:43:55 +0000

    validation finished (kill me now)

--- kashyyk.c
+++ kashyyk.c
@@ -10,13 +10,13 @@
 
 
 // declaring subroutines for later
-int checkIn();
+int checkIn(int currentGuestNo);
 int bookDinner();
 int checkOut();
 int isNameValid(char name[]);
 
 //global variables
-int MAIN[11][2] = {{0, 0}, {0,0}, {0,0}, {0,0},
+int MAIN[12][2] = {{0, 0}, {0,0}, {0,0}, {0,0},
                    {0,0},{0,0},{0,0},{0,0},
                    {0,0},{0,0},{0,0}, {0, 0}};  //empty 2Darray of 11x2
                    // [0] = BookingID, [1] = FirstName, [2] = Surname, [3] = AdultsStaying, [4] = ChildrenStaying,
@@ -55,12 +55,12 @@ int checkIn(int currentGuestNo) {
     int lengthOfStay;
     int adultsStaying;
     int childrenStaying;
-    int maxStaying = 0;
+    int maxStaying;
     int userAge;
     char boardTypeChoice[3]; //0 for BB, 1 for HB, 2 for FB
-    int dailyNewspaper;
     currentGuestNo++; //first check in guest column = 0
-    char confirm[7] = "CONFIRM";
+    char confirm;
+    char newspaperChoice;
 
 
 
@@ -86,78 +86,86 @@ int checkIn(int currentGuestNo) {
     //age
     do {
         printf("\n\nEnter your age: ");
-        scanf("%d", userAge);
+        scanf("%d", &userAge);
         fflush(stdin);
         if (userAge < 16) {
-            printf("\nYou are too young to book, try again");
+            printf("You are too young to book, try again.");
         } else if (userAge > 123) {
-            printf("\nInvalid age choice, try again.");
+            printf("Invalid age choice, try again.");
         } else {
             //confirmation
-            printf("\nYou are %d years old, type 'confirm' to proceed: ", userAge);
-            scanf("%s", &*confirm);
-            *confirm = toupper(*confirm); //make confirm not case-sensitive
-            if (strcmp(confirm, "CONFIRM") == 0) {
-                printf("\nThank you for your booking");
+            printf("\nYou are %d years old, type '/' to proceed: ", userAge);
+            scanf("%c", &confirm);
+            fflush(stdin);
+            if (confirm == '/') {
+                printf("Thank you for your booking");
             } else {
-                printf("\nInput cancelled, try again");
+                printf("Input cancelled, try again");
             }
         }
-    } while(userAge < 16 || userAge > 123 || strcmp(confirm, "CONFIRM") != 0);
+    } while (userAge < 16 || userAge > 123 || confirm != '/');
     MAIN[11][currentGuestNo] = userAge;
 
     // adults staying
     printf("\n\n\t NOTE: the Kashyyyk Hotel can only support 4 guests in one room");
     do {
         do {
-            printf("\nHow many guests are adults (over 16): ");
-            scanf("%d", adultsStaying); //input adults staying
+            printf("\n\nHow many guests are adults (over 16): ");
+            scanf("%d", &adultsStaying); //input adults staying
             fflush(stdin);
             if (adultsStaying > 4) {//too many guests
-                printf("\nYou have entered too many guests, try again.");
+                printf("You have entered too many guests, try again.");
             } else if (adultsStaying < 0) {//too little guests
-                printf("\nYou're choice in invalid, try again.");
+                printf("Your choice is invalid, try again.");
             } else if (adultsStaying == 0) {//no adults
-                printf("\nThere must be at least 1 adult in order to book a room, try again");
+                printf("There must be at least 1 adult in order to book a room, try again");
             } else {
                 //confirmation
-                printf("\nYou are booking for %d adults, type 'confirm' to proceed: ", adultsStaying);
-                scanf("%s", &*confirm);
-                *confirm = toupper(*confirm); //make confirm not case-sensitive
-                if (strcmp(confirm, "CONFIRM") == 0) {
-                    printf("\nThank you for your booking");
+                if (adultsStaying == 1) {
+                    printf("You are booking for 1 adult, type '/' to proceed: ");
                 } else {
-                    printf("\nInput cancelled, try again");
+                    printf("You are booking for %d adults, type '/' to proceed: ", adultsStaying); }
+                scanf("%c", &confirm);
+                fflush(stdin);
+                if (confirm == '/') {
+                    printf("Thank you for your booking.");
+                } else {
+                    printf("Input cancelled, try again");
                 }
             }
-        } while (adultsStaying > 4 || adultsStaying <= 0 || strcmp(confirm, "CONFIRM") != 0);
+        } while (adultsStaying > 4 || adultsStaying <= 0 || confirm != '/');
 
         //children staying
         do {
-            printf("How many guests are children (under 16): ");
-            scanf("%d", childrenStaying); //input adults staying
+            printf("\n\nHow many guests are children (under 16): ");
+            scanf("%d", &childrenStaying); //input adults staying
             fflush(stdin);
             if (childrenStaying > 4) {//too many guests
                 printf("You have entered too many guests, try again.");
             } else if (childrenStaying < 0) {//too little guests
-            printf("You're choice in invalid, try again.");
+                printf("Your choice in invalid, try again.");
             } else {
                 //confirmation
-                printf("You are booking for %d children, type 'confirm' to proceed: ", childrenStaying);
-                scanf("%s", &*confirm);
+                if (childrenStaying == 1) {
+                    printf("You are booking for 1 child, type '/' to proceed: ");
+                } else {
+                    printf("You are booking for %d children, type '/' to proceed: ", childrenStaying); }
+                scanf("%c", &confirm);
                 fflush(stdin);
-                *confirm = toupper(*confirm); //make confirm not case-sensitive
-                if (strcmp(confirm, "CONFIRM") == 0) {
+                if (confirm == '/') {
                     printf("Thank you for your booking");
                 } else {
                     printf("Input cancelled, try again");
                 }
             }
-        } while (childrenStaying > 4 || childrenStaying <= 0 || strcmp(confirm, "CONFIRM") != 0);
+        } while (childrenStaying > 4 || childrenStaying < 0 || confirm != '/');
         maxStaying = adultsStaying + childrenStaying;
+        if (maxStaying > 4) {
+            printf("\n\nYou have booked for too many guests, try again.");
+        }
 
 
-    }   while(maxStaying > 4 || maxStaying <= 0 || strcmp(confirm, "CONFIRM") != 0);
+    } while (maxStaying > 4 || maxStaying <= 0 || confirm != '/');
     MAIN[4][currentGuestNo] = childrenStaying; //store
     MAIN[3][currentGuestNo] = adultsStaying;
 
@@ -166,73 +174,81 @@ int checkIn(int currentGuestNo) {
         printf("\n\nWhat board type would you like? \nType BB for Bed & Breakfast, HB for Half Board and FB for Full Board: ");
         scanf("%s", boardTypeChoice);
         fflush(stdin);
-        if (strcmp(boardTypeChoice, "BB") != 0 && strcmp(boardTypeChoice, "HB") != 0 && strcmp(boardTypeChoice, "FB") != 0) {
+        boardTypeChoice[0] = toupper(boardTypeChoice[0]); boardTypeChoice[1] = toupper(boardTypeChoice[1]);
+        if (strcmp(boardTypeChoice, "BB") != 0 && strcmp(boardTypeChoice, "HB") != 0 &&
+            strcmp(boardTypeChoice, "FB") != 0) {
             printf("\nInvalid board type, try again.");
         } else if (strlen(boardTypeChoice) > 2 || strlen(boardTypeChoice) <= 1) {
             printf("\nInvalid board type, try again");
         } else {
             // confirmation
-            printf("You are booking for Board Option: %s, type 'confirm' to proceed: ", boardTypeChoice);
-            scanf("%s", &*confirm);
+            printf("You are booking for Board Option: %s, type '/' to proceed: ", boardTypeChoice);
+            scanf("%c", &confirm);
             fflush(stdin);
-            *confirm = toupper(*confirm); //make confirm not case-sensitive
-            if (strcmp(confirm, "CONFIRM") == 0) {
+            if (confirm == '/') {
                 printf("Thank you for your booking");
             } else {
                 printf("Input cancelled, try again");
             } //Boardtypechoice will equal BB, HB, or FB
         }
-    } while (strlen(boardTypeChoice) > 2 || strlen(boardTypeChoice) <= 1 || strcmp(confirm, "CONFIRM") != 0);
-    MAIN[5][currentGuestNo] = (boardTypeChoice == "BB") ? 3 : (boardTypeChoice == "HB") ? 2 : (boardTypeChoice == "FB") ? 1: 0;
+    } while (strlen(boardTypeChoice) > 2 || strlen(boardTypeChoice) <= 1 || confirm != '/' ||
+            (strcmp(boardTypeChoice, "BB") != 0 && strcmp(boardTypeChoice, "HB") != 0 && strcmp(boardTypeChoice, "FB") != 0));
+    MAIN[5][currentGuestNo] = (strcmp(boardTypeChoice, "BB") == 0) ? 3 : (strcmp(boardTypeChoice, "HB") == 0) ? 2 :
+            (strcmp(boardTypeChoice, "FB") == 0) ? 1 : 0;
     // this is an easier way of writing a massive if-else statement
     // if BB, array stores 3, if HB, array stores 2, if FB, array stores 1
 
     //lengthOfStay
-    do{
-        printf("Please enter the length of your stay in days: ");
-        scanf("%d", lengthOfStay); //input length of stay, no need to check if it is digits, as scanf already handles it
+    do {
+        printf("\n\nPlease enter the length of your stay in days: ");
+        scanf("%d", &lengthOfStay); //input length of stay, no need to check if it is digits, as scanf already handles it
         fflush(stdin);
         if (lengthOfStay <= 0) {
             printf("Your choice is invalid, Try again.");
         } else if (lengthOfStay > 14) {
             printf("The maximum time a room can be booked is 14 days. Try again.");
         } else {
-            printf("You are booking for %d days, type 'confirm' to proceed: ", lengthOfStay);
-            scanf("%s", &*confirm);
-            *confirm = toupper(*confirm); //make confirm not case-sensitive
-            if (strcmp(confirm, "CONFIRM") == 0)
+            if (lengthOfStay == 1) {
+                printf("You are booking for 1 day, type '/' to proceed: ");
+            } else {
+                printf("You are booking for %d days, type '/' to proceed: ", lengthOfStay); }
+            scanf("%c", &confirm);
+            fflush(stdin);
+            if (confirm == '/')
                 printf("Thank you for your booking, we hope your enjoy your stay.");
             else
                 printf("Input cancelled, try again");
         }
-    } while(lengthOfStay > 14 || lengthOfStay <= 0 || strcmp(confirm, "CONFIRM") != 0); //while lengthofstay is in range and user has confirmed
+    } while (lengthOfStay > 14 || lengthOfStay <= 0 ||
+             confirm != '/'); //while lengthofstay is in range and user has confirmed
     MAIN[6][currentGuestNo] = lengthOfStay;
 
     //daily newspaper
+
     do {
-        int newspaper2 = 0; //Variable to exit for while loop
-        char newspaper[3]; //array to check if Yes or No
-        while (newspaper2 = 0) {
-            printf("\nWould you like to opt for our daily newspaper, this service has a one-off charge of £5.50:");
-            scanf("%s", &newspaper);
-            fflush(stdin);
-            *newspaper = toupper(*newspaper); //Making input not case sensitive
-            if  (strcmp(newspaper, "YES") == 0) {
-                printf("\nYour newspaper will be with you shortly, and you will be charged at the end of your stay.");
-                newspaper2 = 2;
-            } else if (strcmp(newspaper, "NO") == 0) {
-                printf("\nOkay, please enjoy your stay.");
-                newspaper2 = 1;
-            } else {
-            printf("\nPlease enter yes or no:");
-            newspaper2 = 0; } }
-        while(newspaper2 = 2 || strcmp(newspaper, "YES") != 0); //storing 1 or 0 in array, can be pulled from array to see if added onto final price at checkout
-        MAIN[7][1] = 1;
+        //also we do not need a second while loop as the do-while can handle looping
+        // char newspaperChoice; //instead of a string we can use characters (easier to deal with)
+        printf("\n\nWould you like to opt for our daily newspaper, this service has a one-off charge of £5.50.");
+        printf("Enter 'Y' for yes and 'N' for no: ");
+        scanf("%c", &newspaperChoice);
+        fflush(stdin);
+        newspaperChoice = toupper(newspaperChoice);
+        if (newspaperChoice == 'Y') {
+            printf("Your newspapers will be delivered daily, thank you for choosing our service");
+        } else if (newspaperChoice == 'N') {
+            printf("You will not be charged with newspapers, enjoy your stay");
+        } else {
+            printf("Invalid choice, try again");
         }
+    } while (newspaperChoice != 'Y' && newspaperChoice != 'N'); //while newspaper choice isn't valid
+    MAIN[7][currentGuestNo] = newspaperChoice == 'Y' ? 1 : 0; //1 = yes, 0 = no
 
-
-
+    for (int i = 0; i < 11; i++) {
+        printf("%s", MAIN[i][currentGuestNo]);
+    }
     //CALCULATION (boardRate and roomRate)
+
+    return 0;
 }
 
 
