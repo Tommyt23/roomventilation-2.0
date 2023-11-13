c8b649b12929eb9c6249f4c7413a22669969dc79
Tom Thurston <t.thurston@students.reading.ac.uk>
2023-11-13 15:33:52 +0000

    add instructions

--- codeformusic.c
+++ codeformusic.c
@@ -8,7 +8,7 @@
 DWORD WINAPI PlaySoundThread(LPVOID lpParam) {
     // Use SND_FILENAME to indicate that the first parameter is a file name
     // Use SND_ASYNC to play the sound asynchronously
-    PlaySound(TEXT("C:\\Users\\Tommy\\CLionProjects\\untitled2\\for-elevator-jazz-music-124005.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+    PlaySound(TEXT(/* --- PUT MUSIC FILE LOCATION BETWEEN THE STRING MARKERS --- */""), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 
     return 0;
 }
@@ -17,7 +17,7 @@ int main() {
     // Create a thread to play the sound
     HANDLE soundThread = CreateThread(NULL, 0, PlaySoundThread, NULL, 0, NULL);
 
-    /*ADD MAIN THREAD CODE HERE*/
+    /* --- ADD MAIN THREAD CODE HERE --- */
 
     // Optional, wait for the user to close the program
     printf("Press Enter to exit.\n");
